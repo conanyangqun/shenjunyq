459c95dfbd2b26d415422449a197412cbacb5df4
yangqun <yangqunxinli@163.com>
2021-08-22 17:40:29 +0800

    build site with jekyll

---
+++ run.sh
@@ -0,0 +1,29 @@
+export JEKYLL_VERSION=latest
+
+docker run --rm \
+  --volume="$PWD:/srv/jekyll" \
+  -it jekyll/jekyll:$JEKYLL_VERSION \
+  jekyll build
+
+docker run --rm \
+  --volume="$PWD:/srv/jekyll" \
+  -it jekyll/jekyll:$JEKYLL_VERSION \
+  bundle
+
+docker run -d --rm \
+  --volume="$PWD:/srv/jekyll" \
+  --publish 4000:4000 \
+  jekyll/jekyll:$JEKYLL_VERSION \
+  jekyll serve
+
+docker run --rm \
+  --volume="$PWD:/srv/jekyll" \
+  -it jekyll/jekyll:$JEKYLL_VERSION \
+  jekyll new
+
+docker run --rm \
+  --volume="$PWD:/srv/jekyll" \
+  --publish 4000:4000 \
+  -e "TZ=Asia/Shanghai" \
+  -it jekyll/jekyll:$JEKYLL_VERSION \
+  /bin/bash
