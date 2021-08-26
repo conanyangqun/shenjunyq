65681692e9d00942b5a53eb19100334670dc2cdf
yangqun <yangqunxinli@163.com>
2021-08-27 00:08:02 +0800

    add 2021-08-27-我与生信.md

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210825.2237';
+const cacheName = 'chirpy-20210827.0007';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
