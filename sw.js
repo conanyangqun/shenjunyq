5e2aa8f739ed1f932e1e701709632a801568f4f4
yangqun <yangqunxinli@163.com>
2021-08-28 23:19:52 +0800

    add google search console.

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210828.2313';
+const cacheName = 'chirpy-20210828.2319';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
