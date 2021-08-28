40d52a04bc992f4d806a4cd1871dedf51aaf2577
yangqun <yangqunxinli@163.com>
2021-08-28 22:59:25 +0800

    change theme, add disqus.

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210827.0007';
+const cacheName = 'chirpy-20210828.2257';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
